51c59967b9ff4f5d42279f878b8f7f804d5d8d1d
Tedzury <asdrogachev@gmail.com>
2023-04-30 00:08:50 +0300

    feat: prepare object with keys info. Try basic functionality

--- js/index.js
+++ js/index.js
@@ -1,13 +1,659 @@
-const keys = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
+const keyboard = {
+  Backquote: {
+    ru: {
+      main: 'ё',
+      shift: 'Ё',
+    },
+    en: {
+      main: '`',
+      shift: '~',
+    },
+  },
+  Digit1: {
+    ru: {
+      main: '1',
+      shift: '!',
+    },
+    en: {
+      main: '1',
+      shift: '!',
+    },
+  },
+  Digit2: {
+    ru: {
+      main: '2',
+      shift: '"',
+    },
+    en: {
+      main: '2',
+      shift: '@',
+    },
+  },
+  Digit3: {
+    ru: {
+      main: '3',
+      shift: '№',
+    },
+    en: {
+      main: '3',
+      shift: '#',
+    },
+  },
+  Digit4: {
+    ru: {
+      main: '4',
+      shift: ';',
+    },
+    en: {
+      main: '4',
+      shift: '$',
+    },
+  },
+  Digit5: {
+    ru: {
+      main: '5',
+      shift: '%',
+    },
+    en: {
+      main: '5',
+      shift: '%',
+    },
+  },
+  Digit6: {
+    ru: {
+      main: '6',
+      shift: ':',
+    },
+    en: {
+      main: '6',
+      shift: '^',
+    },
+  },
+  Digit7: {
+    ru: {
+      main: '7',
+      shift: '?',
+    },
+    en: {
+      main: '7',
+      shift: '&',
+    },
+  },
+  Digit8: {
+    ru: {
+      main: '8',
+      shift: '*',
+    },
+    en: {
+      main: '8',
+      shift: '*',
+    },
+  },
+  Digit9: {
+    ru: {
+      main: '9',
+      shift: '(',
+    },
+    en: {
+      main: '9',
+      shift: '(',
+    },
+  },
+  Digit0: {
+    ru: {
+      main: '0',
+      shift: ')',
+    },
+    en: {
+      main: '0',
+      shift: ')',
+    },
+  },
+  Minus: {
+    ru: {
+      main: '-',
+      shift: '_',
+    },
+    en: {
+      main: '-',
+      shift: '_',
+    },
+  },
+  Equal: {
+    ru: {
+      main: '=',
+      shift: '+',
+    },
+    en: {
+      main: '=',
+      shift: '+',
+    },
+  },
+  Backspace: {
+    ru: {
+      main: 'Backspace',
+      shift: 'Backspace',
+    },
+    en: {
+      main: 'Backspace',
+      shift: 'Backspace',
+    },
+  },
+  Tab: {
+    ru: {
+      main: 'Tab',
+      shift: 'Tab',
+    },
+    en: {
+      main: 'Tab',
+      shift: 'Tab',
+    },
+  },
+  KeyQ: {
+    ru: {
+      main: 'й',
+      shift: 'Й',
+    },
+    en: {
+      main: 'q',
+      shift: 'Q',
+    },
+  },
+  KeyW: {
+    ru: {
+      main: 'ц',
+      shift: 'Ц',
+    },
+    en: {
+      main: 'w',
+      shift: 'W',
+    },
+  },
+  KeyE: {
+    ru: {
+      main: 'у',
+      shift: 'У',
+    },
+    en: {
+      main: 'e',
+      shift: 'E',
+    },
+  },
+  KeyR: {
+    ru: {
+      main: 'к',
+      shift: 'К',
+    },
+    en: {
+      main: 'r',
+      shift: 'R',
+    },
+  },
+  KeyT: {
+    ru: {
+      main: 'е',
+      shift: 'Е',
+    },
+    en: {
+      main: 't',
+      shift: 'T',
+    },
+  },
+  KeyY: {
+    ru: {
+      main: 'н',
+      shift: 'Н',
+    },
+    en: {
+      main: 'y',
+      shift: 'Y',
+    },
+  },
+  KeyU: {
+    ru: {
+      main: 'г',
+      shift: 'Г',
+    },
+    en: {
+      main: 'u',
+      shift: 'U',
+    },
+  },
+  KeyI: {
+    ru: {
+      main: 'ш',
+      shift: 'Ш',
+    },
+    en: {
+      main: 'i',
+      shift: 'I',
+    },
+  },
+  KeyO: {
+    ru: {
+      main: 'щ',
+      shift: 'Щ',
+    },
+    en: {
+      main: 'o',
+      shift: 'O',
+    },
+  },
+  KeyP: {
+    ru: {
+      main: 'з',
+      shift: 'З',
+    },
+    en: {
+      main: 'p',
+      shift: 'P',
+    },
+  },
+  BracketLeft: {
+    ru: {
+      main: 'х',
+      shift: 'Х',
+    },
+    en: {
+      main: '[',
+      shift: '{',
+    },
+  },
+  BracketRight: {
+    ru: {
+      main: 'ъ',
+      shift: 'Ъ',
+    },
+    en: {
+      main: ']',
+      shift: '}',
+    },
+  },
+  Backslash: {
+    ru: {
+      main: '\\',
+      shift: '/',
+    },
+    en: {
+      main: '\\',
+      shift: '|',
+    },
+  },
+  Delete: {
+    ru: {
+      main: 'Delete',
+      shift: 'Delete',
+    },
+    en: {
+      main: 'Delete',
+      shift: 'Delete',
+    },
+  },
+  CapsLock: {
+    ru: {
+      main: 'CapsLock',
+      shift: 'CapsLock',
+    },
+    en: {
+      main: 'CapsLock',
+      shift: 'CapsLock',
+    },
+  },
+  KeyA: {
+    ru: {
+      main: 'ф',
+      shift: 'Ф',
+    },
+    en: {
+      main: 'a',
+      shift: 'A',
+    },
+  },
+  KeyS: {
+    ru: {
+      main: 'ы',
+      shift: 'Ы',
+    },
+    en: {
+      main: 's',
+      shift: 'S',
+    },
+  },
+  KeyD: {
+    ru: {
+      main: 'в',
+      shift: 'В',
+    },
+    en: {
+      main: 'd',
+      shift: 'D',
+    },
+  },
+  KeyF: {
+    ru: {
+      main: 'а',
+      shift: 'А',
+    },
+    en: {
+      main: 'f',
+      shift: 'F',
+    },
+  },
+  KeyG: {
+    ru: {
+      main: 'п',
+      shift: 'П',
+    },
+    en: {
+      main: 'g',
+      shift: 'G',
+    },
+  },
+  KeyH: {
+    ru: {
+      main: 'р',
+      shift: 'Р',
+    },
+    en: {
+      main: 'h',
+      shift: 'H',
+    },
+  },
+  KeyJ: {
+    ru: {
+      main: 'о',
+      shift: 'О',
+    },
+    en: {
+      main: 'j',
+      shift: 'J',
+    },
+  },
+  KeyK: {
+    ru: {
+      main: 'л',
+      shift: 'Л',
+    },
+    en: {
+      main: 'k',
+      shift: 'K',
+    },
+  },
+  KeyL: {
+    ru: {
+      main: 'д',
+      shift: 'Д',
+    },
+    en: {
+      main: 'l',
+      shift: 'L',
+    },
+  },
+  Semicolon: {
+    ru: {
+      main: 'ж',
+      shift: 'Ж',
+    },
+    en: {
+      main: ';',
+      shift: ':',
+    },
+  },
+  Quote: {
+    ru: {
+      main: 'э',
+      shift: 'Э',
+    },
+    en: {
+      main: '\'',
+      shift: '"',
+    },
+  },
+  Enter: {
+    ru: {
+      main: 'Enter',
+      shift: 'Enter',
+    },
+    en: {
+      main: 'Enter',
+      shift: 'Enter',
+    },
+  },
+  ShiftLeft: {
+    ru: {
+      main: 'Shift',
+      shift: 'Shift',
+    },
+    en: {
+      main: 'Shift',
+      shift: 'Shift',
+    },
+  },
+  KeyZ: {
+    ru: {
+      main: 'я',
+      shift: 'Я',
+    },
+    en: {
+      main: 'z',
+      shift: 'Z',
+    },
+  },
+  KeyX: {
+    ru: {
+      main: 'ч',
+      shift: 'Ч',
+    },
+    en: {
+      main: 'x',
+      shift: 'X',
+    },
+  },
+  KeyC: {
+    ru: {
+      main: 'с',
+      shift: 'С',
+    },
+    en: {
+      main: 'c',
+      shift: 'C',
+    },
+  },
+  KeyV: {
+    ru: {
+      main: 'м',
+      shift: 'М',
+    },
+    en: {
+      main: 'v',
+      shift: 'V',
+    },
+  },
+  KeyB: {
+    ru: {
+      main: 'и',
+      shift: 'И',
+    },
+    en: {
+      main: 'b',
+      shift: 'B',
+    },
+  },
+  KeyN: {
+    ru: {
+      main: 'т',
+      shift: 'Т',
+    },
+    en: {
+      main: 'n',
+      shift: 'N',
+    },
+  },
+  KeyM: {
+    ru: {
+      main: 'ь',
+      shift: 'Ь',
+    },
+    en: {
+      main: 'm',
+      shift: 'M',
+    },
+  },
+  Comma: {
+    ru: {
+      main: 'б',
+      shift: 'Б',
+    },
+    en: {
+      main: ',',
+      shift: '<',
+    },
+  },
+  Period: {
+    ru: {
+      main: 'ю',
+      shift: 'Ю',
+    },
+    en: {
+      main: '.',
+      shift: '>',
+    },
+  },
+  Slash: {
+    ru: {
+      main: '.',
+      shift: ',',
+    },
+    en: {
+      main: '/',
+      shift: '?',
+    },
+  },
+  ArrowUp: {
+    ru: {
+      main: '↑',
+      shift: '↑',
+    },
+    en: {
+      main: '↑',
+      shift: '↑',
+    },
+  },
+  ShiftRight: {
+    ru: {
+      main: 'Shift',
+      shift: 'Shift',
+    },
+    en: {
+      main: 'Shift',
+      shift: 'Shift',
+    },
+  },
+  ControlLeft: {
+    ru: {
+      main: 'Ctrl',
+      shift: 'Ctrl',
+    },
+    en: {
+      main: 'Ctrl',
+      shift: 'Ctrl',
+    },
+  },
+  MetaLeft: {
+    ru: {
+      main: 'Win',
+      shift: 'Win',
+    },
+    en: {
+      main: 'Win',
+      shift: 'Win',
+    },
+  },
+  AltLeft: {
+    ru: {
+      main: 'Alt',
+      shift: 'Alt',
+    },
+    en: {
+      main: 'Alt',
+      shift: 'Alt',
+    },
+  },
+  Space: {
+    ru: {
+      main: 'Space',
+      shift: 'Space',
+    },
+    en: {
+      main: 'Space',
+      shift: 'Space',
+    },
+  },
+  AltRight: {
+    ru: {
+      main: 'Alt',
+      shift: 'Alt',
+    },
+    en: {
+      main: 'Alt',
+      shift: 'Alt',
+    },
+  },
+  ArrowLeft: {
+    ru: {
+      main: '←',
+      shift: '←',
+    },
+    en: {
+      main: '←',
+      shift: '←',
+    },
+  },
+  ArrowDown: {
+    ru: {
+      main: '↓',
+      shift: '↓',
+    },
+    en: {
+      main: '↓',
+      shift: '↓',
+    },
+  },
+  ArrowRight: {
+    ru: {
+      main: '→',
+      shift: '→',
+    },
+    en: {
+      main: '→',
+      shift: '→',
+    },
+  },
+  ControlRight: {
+    ru: {
+      main: 'Ctrl',
+      shift: 'Ctrl',
+    },
+    en: {
+      main: 'Ctrl',
+      shift: 'Ctrl',
+    },
+  },
+};
 
-const wrapper = document.createElement('div');
-wrapper.classList.add('key-wrapp');
+const lang = 'en';
+const isShift = 'main';
 
-keys.forEach((item) => {
-  const key = document.createElement('div');
-  key.classList.add('key');
-  key.textContent = item;
-  wrapper.append(key);
+const body = document.querySelector('#body');
+
+const keyboardWrapper = document.createElement('div');
+keyboardWrapper.classList.add('key-wrapp');
+
+Object.keys(keyboard).forEach((key) => {
+  const keyBoardKey = document.createElement('div');
+  keyBoardKey.classList.add('key');
+  keyBoardKey.textContent = keyboard[key][lang][isShift];
+  keyboardWrapper.append(keyBoardKey);
 });
 
-document.body.append(wrapper);
+body.append(keyboardWrapper);
