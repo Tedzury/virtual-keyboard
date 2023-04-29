f56ec396f25266c2f20711725d04797dc51c29d2
Tedzury <asdrogachev@gmail.com>
2023-04-29 17:55:57 +0300

    feat: install and configure eslint with airbnb-base

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "airbnb-base",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
